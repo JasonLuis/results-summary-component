57fd0da489b27e6796c40b77eed5a7e70e4131b1
JasonLuis <jason20luis@gmail.com>
2023-03-02 21:05:39 -0300

    refactor: add title in my home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,6 +4,9 @@
 
 <script setup lang="ts">
 import UiResultSummary from '~/core/components/ResultSummary/ResultSummary.vue';
+useHead({
+  title: 'Result Summary'
+});
 </script>
 
 <style scoped></style>
